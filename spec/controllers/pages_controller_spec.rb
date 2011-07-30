30eae308e5df3b08b40d1118121eb8bd666813ac
pcp135 <phil@parsons.uk.com>
2011-07-30 19:28:26 -0400

    Fixed rspec and updated gemfile

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,14 +3,19 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before (:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => @base_title + " | Home")
     end
   end
 
@@ -19,9 +24,10 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
 
@@ -30,9 +36,10 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => @base_title + " | About")
     end
   end
 
@@ -41,9 +48,10 @@ describe PagesController do
       get 'help'
       response.should be_success
     end
+
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Help")
+      response.should have_selector("title", :content => @base_title + " | Help")
     end
   end
 
