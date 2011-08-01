d9e8ff69725d283255526f260f9158e6cd60fd7c
pcp135 <phil@parsons.uk.com>
2011-07-31 22:55:55 -0400

    added extra checks

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Signup")
+  end
+
   it "should have a Home Page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
