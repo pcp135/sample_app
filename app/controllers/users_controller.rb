a7f99c98010e28e70a5c38fa2c9658cff5a513f7
pcp135 <phil@parsons.uk.com>
2011-08-07 17:01:55 -0400

    done with user edit/update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, 
+                                         :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
 
   def show
     @user = User.find(params[:id])
@@ -10,6 +12,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     @title = "Signup"
+  end
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
   end
 
   def create
@@ -40,6 +52,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
+
   private
   def authenticate
     deny_access unless signed_in?
@@ -48,6 +66,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
   end
 
 end
