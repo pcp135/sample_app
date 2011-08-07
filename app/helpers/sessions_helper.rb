d44a3278e82e1c01caa00024c30bf25a50694db5
pcp135 <phil@parsons.uk.com>
2011-08-07 10:19:50 -0400

    added basic authentication

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -31,5 +31,14 @@ module SessionsHelper
   def remember_token
     cookies.signed[:remember_token] || [nil, nil]
   end
+  
+  def deny_access
+    redirect_to signin_path, 
+    :notice => "Please sign in to access this page."
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
 
 end
