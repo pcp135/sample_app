f1bb2559c55ade19e56d8869da318595aab65205
pcp135 <phil@parsons.uk.com>
2011-08-07 10:30:45 -0400

    added redirect back

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,6 +22,17 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def deny_access
+    store_location
+    redirect_to signin_path, 
+    :notice => "Please sign in to access this page."
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
   private
   
   def user_from_remember_token
@@ -32,13 +43,15 @@ module SessionsHelper
     cookies.signed[:remember_token] || [nil, nil]
   end
   
-  def deny_access
-    redirect_to signin_path, 
-    :notice => "Please sign in to access this page."
-  end
-
   def current_user?(user)
     user == current_user
   end
 
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  def clear_return_to
+    session[:return_to] = nil
+  end
 end
