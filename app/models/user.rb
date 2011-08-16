c3ff5c8bded1e43f787ef99c3bb7cedefab13671
pcp135 <phil@parsons.uk.com>
2011-08-16 00:57:19 -0400

    made first round of changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,17 +17,24 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   has_many :microposts, :dependent => :destroy
+  has_many(:relationships, :foreign_key => "follower_id",
+           :dependent => :destroy)
+  has_many(:following, :through => :relationships, 
+           :source => :followed)
+  has_many(:reverse_relationships, :foreign_key => "followed_id",
+           :class_name => "Relationship", :dependent => :destroy)
+  has_many(:followers, :through => :reverse_relationships,
+           :source => :follower)
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :name, :presence => true, 
-  :length => { :maximum => 50}
-  validates :email, :presence => true, 
-  :format => { :with => email_regex }, 
-  :uniqueness => { :case_sensitive => false }
-  validates :password, :presence => true, 
-  :confirmation => true, 
-  :length => { :within => 6..40 }
+  validates :name, :presence => true, :length => { :maximum => 50}
+  validates(:email, :presence => true, 
+             :format => { :with => email_regex }, 
+             :uniqueness => { :case_sensitive => false })
+  validates(:password, :presence => true, 
+             :confirmation => true, 
+             :length => { :within => 6..40 })
   
   before_save :encrypt_password
 
@@ -48,6 +55,18 @@ class User < ActiveRecord::Base
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
+  end
+
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 SampleApp::Application.routes.draw do
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   get "sessions/new"
 
-  resources :users
   resources :sessions, :only => [:new, :create, :destroy]
   resources :microposts, :only => [:create, :destroy]
-  
+  resources :relationships, :only => [:create, :destroy]
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
