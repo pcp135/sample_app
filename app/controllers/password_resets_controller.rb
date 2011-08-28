6726f0e4d5e4b501246ac9af3c88875188954d96
pcp135 <phil@parsons.uk.com>
2011-08-28 16:38:46 -0400

    set password reset to log user in

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,6 +17,7 @@ class PasswordResetsController < ApplicationController
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset has expired!  Please try again"
     elsif @user.update_attributes(params[:user])
+      sign_in @user
       redirect_to root_url, :notice => "Password has been reset"
     else
       render :edit
